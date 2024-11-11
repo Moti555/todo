0817422859ed787639afbf2a6f6c46dc53a73cb2
Mika Lohi <152972334+Moti555@users.noreply.github.com>
2024-11-11 19:53:43 +0200

    ji

--- todo/server/index.test.js
+++ todo/server/index.test.js
@@ -15,7 +15,7 @@ describe('GET TASKS',() => {
         const response = await fetch('http://localhost:3001/');
         const data = await response.json();
 
-        expect(response.status).to.equal(200); //vaihda takas 200
+        expect(response.status).to.equal(200); 
         expect(data).to.be.an('array').that.is.not.empty;
         expect(data[0]).to.include.all.keys('id','description');
     })
